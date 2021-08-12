59d27f0579917e6a7e0dfd4b534b4bb3686aa729
Santiago Betancur <santiago-betancur@hotmail.com>
2021-08-12 00:56:56 -0500

    Metodos de readString y writeString, Vinculados a java 11 para su utilización en archivos, utilizando  StandarOpenOperation

--- src/main/java/com/developer/programacionfuncional/variablemetodostring/VariableMetodosString.java
+++ src/main/java/com/developer/programacionfuncional/variablemetodostring/VariableMetodosString.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.developer.programacionfuncional;
+package com.developer.programacionfuncional.variablemetodostring;
 
 import java.util.stream.Collectors;
 
